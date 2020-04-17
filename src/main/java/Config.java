7773d82e9c8af694c1ab706330686e46e8530237
unknown <jailson.alves@quicard.com.br>
2020-04-17 17:10:01 -0300

    add Config

---
+++ src/main/java/Config.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Config {
+
+    private static Map<String, String> mapaParametros = new HashMap<String, String>();
+
+    private static String HOST="localhost";
+    private static String USER="guest";
+    private static String PASSWORD="guest";
+    private static String PORT="15672";
+
+    //receive
+    private static String QUEUE_NAME="queue.fila2";
+    private static String EXCHANGE_NAME="";
+
+    //send
+    private static String QUEUE_SEND_NAME="queue.fila1";
+    private static String EXCHANGE_SEND_NAME="exchange.exchange1";
+
+    public Config(Map<String, String> mapaParametros) {
+        this.mapaParametros.putAll(mapaParametros);
+        printCfgs();
+    }
+
+    public static String getHost() {
+        return mapaParametros.containsKey("HOST")?mapaParametros.get("HOST"):HOST;
+    }
+
+    public static String getUser() {
+        return mapaParametros.containsKey("USER")?mapaParametros.get("USER"):USER;
+    }
+
+    public static String getPassword() {
+        return mapaParametros.containsKey("PASSWORD")?mapaParametros.get("PASSWORD"):PASSWORD;
+    }
+
+    public static int getPort() {
+        return Integer.parseInt(mapaParametros.containsKey("PORT")?mapaParametros.get("PORT"):PORT);
+    }
+
+    public static String getQueueName() {
+        if (mapaParametros.get("METHOD").equalsIgnoreCase("SEND")) {
+            return mapaParametros.containsKey("QUEUE_NAME") ? mapaParametros.get("QUEUE_NAME") : QUEUE_SEND_NAME;
+        } else {
+            return mapaParametros.containsKey("QUEUE_NAME") ? mapaParametros.get("QUEUE_NAME") : QUEUE_NAME;
+        }
+    }
+
+    public static String getExchangeName() {
+        if (mapaParametros.get("METHOD").equalsIgnoreCase("SEND")) {
+            return mapaParametros.containsKey("EXCHANGE_NAME") ? mapaParametros.get("EXCHANGE_NAME") : EXCHANGE_SEND_NAME;
+        } else {
+            return mapaParametros.containsKey("EXCHANGE_NAME") ? mapaParametros.get("EXCHANGE_NAME") : EXCHANGE_NAME;
+        }
+    }
+
+    public static String getMessage() {
+        return mapaParametros.get("MESSAGE");
+    }
+
+    private void printCfgs() {
+        System.out.println("HOST                        :"+getHost());
+        System.out.println("USER                        :"+getUser());
+        System.out.println("PASSWORD                    :"+getPassword());
+        System.out.println("PORT                        :"+getPort());
+        System.out.println("QUEUE_NAME                  :"+getQueueName());
+        System.out.println("EXCHANGE_NAME               :"+getExchangeName());
+    }
+
+}
